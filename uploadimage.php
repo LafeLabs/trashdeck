6697bed991cd2ff042186cb776bdcd665ce8ede9
Trash Robot <lafelabs@gmail.com>
2023-11-13 20:52:22 -0700

    trashdeck image load works now

--- uploadimage.php
+++ uploadimage.php
@@ -4,14 +4,14 @@
     $imageIndex =  count($files) - 1;
     $infilename = basename( $_FILES["fileToUpload"]["name"]);
     $extension = substr($infilename,-4);
-    $target_file = $target_dir . "image" . $imageIndex . $extension;
+    $target_file = $target_dir . "upload" . $imageIndex . $extension;
     $uploadOk = 1;
     
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     // Check if file already exists
     if (file_exists($target_file)) {
         $imageIndex +=  1;
-        $target_file = $target_dir . "image" . $imageIndex . $extension;
+        $target_file = $target_dir . "upload" . $imageIndex . $extension;
     }
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
@@ -29,7 +29,4 @@
         <a href = "index.html">index.html</a>
     </p>
     
-    <p style = "font-size:5em">
-        <a href = "images.html">images.html</a>
-    </p>
-    
+
